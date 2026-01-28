237d7b50162110a9d145d8d5159e0230ae9a1c81
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2026-01-28 21:08:33 +0100

    Initialize a new Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+/*
+ * This file is part of junit-parallelization-recipes
+ *
+ * Copyright (c) 2026 Thomas Himmelstoss
+ *
+ * This software is subject to the MIT license. You should have
+ * received a copy of the license along with this program.
+ */
+
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "junit-parallelization-recipes"
+
